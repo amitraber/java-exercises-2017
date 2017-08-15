28f75c0b0312a56477a69eee3bcecc31ed4dd64b
borka-PC\borka <boris.goman@gmail.com>
2017-08-15 23:06:40 +0300

    classes

---
+++ src/main/java/il/co/electriccollege/triathlon/animals/Animals.java
@@ -0,0 +1,7 @@
+package il.co.electriccollege.triathlon.animals;
+
+/**
+ * Created by borka on 8/15/2017.
+ */
+public class Animals {
+}
---
+++ src/main/java/il/co/electriccollege/triathlon/vehicles/Airplains.java
@@ -0,0 +1,7 @@
+package il.co.electriccollege.triathlon.vehicles;
+
+/**
+ * Created by borka on 8/15/2017.
+ */
+public class Airplains {
+}
